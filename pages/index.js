17530034d5a66998f60414c69e9547ab1aa49d65
Quatrefoil <58088988+Q4FOIL@users.noreply.github.com>
2021-11-21 16:58:11 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Quatrefoil</a>
+          Quatrefoil
         </h1>
 
         <p className={styles.description}>
